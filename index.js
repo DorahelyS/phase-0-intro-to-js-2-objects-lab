42bcf68c38a51b3224586f508c40858e5b72ae57
Dorahely Sanchez <dorahelys@gmail.com>
2023-10-12 12:57:46 -0500

    Plays with objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+name: "Dorahey",
+streetAddress: "2159 California Street",
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {...object }
+    newEmployee[key] = value
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value
+   return object
+  }
+
+  function deleteFromEmployeeByKey(object, key, value){
+    const newEmployee = {...object }
+    delete newEmployee[key] 
+    return newEmployee
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(object, key, value){
+    object[key] = value
+    delete object[key]
+    return object
+  }
+  
